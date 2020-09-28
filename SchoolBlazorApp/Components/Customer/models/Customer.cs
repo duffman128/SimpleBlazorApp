cccedafc744a5a2863ccecde8077a52790e1fcce
duffman128 <38553541+duffman128@users.noreply.github.com>
2020-09-28 12:25:29 +0200

    Update Customer.cs
    removed unnecessary annotations.

--- SchoolBlazorApp/Components/Customer/models/Customer.cs
+++ SchoolBlazorApp/Components/Customer/models/Customer.cs
@@ -1,5 +1,3 @@
-﻿using System.ComponentModel.DataAnnotations;
-
 namespace SchoolBlazorApp.Components.Customer.models
 {
     public class CustomerModel : ICustomer
@@ -12,16 +10,10 @@ namespace SchoolBlazorApp.Components.Customer.models
 
         public int Id { get; set; }
 
-        [Required(ErrorMessage = "First name is required")]
-        [StringLength(16, MinimumLength = 2, ErrorMessage = "Length must be between 2 and 16 characters")]
         public string FirstName { get; set; }
 
-        [Required(ErrorMessage = "Last name is required")]
-        [StringLength(16, MinimumLength = 2, ErrorMessage = "Length must be between 2 and 16 characters")]
         public string LastName { get; set; }
 
-        [Required(ErrorMessage = "Resident is required")]
-        [Display(Name = "Resident")]
         public bool IsResident { get; set; }
 
         public float TaxAmount { get; set; }
